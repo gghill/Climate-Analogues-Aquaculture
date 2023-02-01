3c38a1f3ee2af4a4a6f4653818535171e028280f
Amy Leigh Mackintosh <amy.mackintosh@nord.no>
2023-02-01 21:12:08 +0100

    A slightly better map at the bottom

--- Scripts/produceAnalogs_Amy.R
+++ Scripts/produceAnalogs_Amy.R
@@ -75,26 +75,10 @@ names(sal1)[names(sal1) == 'x'] <- 'long'
 names(sal1)[names(sal1) == 'y'] <- 'lat'
 
 
-# please ignore, I'm working on this
-current <- data.frame(long = sal1$long, lat = sal1$lat)
-current$climate <- "current"
-novel <- data.frame(long = sal1$analogNovelty.x, lat = sal1$analogNovelty.y)
-novel$climate <- "novel"
-dissappearing <- data.frame(long = sal1$analogDisappearance.x, lat = sal1$analogDisappearance.y)
-dissappearing$climate <- "dissappearing"
-
-dat <- merge(current, novel, dissappearing, by.y = "long", all.x = TRUE, all.y = TRUE)
-
-merge
-
-
-countries <- world(path = "../Data/countries")
-
-world <- map_data("world")
-
-
 
 # Salmo maps ----
+
+countries <- world(path = "../Data/countries")
 
 download_window <- ext(c(-60, 60, 50, 80))
 plot(countries, ext = download_window)
@@ -102,28 +86,55 @@ points(salar585_subset[ , c("x", "y")], pch = 20, col = "red")
 points(salar585_subset[ , c("analogNovelty.x", "analogNovelty.y")], pch = 20, col = "blue")
 points(salar585_subset[ , c("analogDisappearance.x", "analogDisappearance.y")], pch = 20, col = "green")
 
-  
+
+# ggplot
+world <- map_data("world")  
+
 sal <- ggplot() +
   geom_map(
     data = world, map = world,
     aes(map_id = region),
     color = "grey", fill = "lightgray", size = 0.01
   ) +
-  geom_point(data=sal1, aes(x = long, y = lat), color = "darkred") +
-  geom_point(data=sal1, aes(x = analogNovelty.x, y = analogNovelty.y), color = "blue") +
-  geom_point(data=sal1, aes(x = analogDisappearance.x, y = analogDisappearance.y), color = "darkgreen") +
-  xlim(-60, 60) + ylim(50, 80) +
-  theme(legend.position = "right")
+  geom_point(data=sal1, x = long, y = lat, aes(color = "darkred")) +
+  geom_point(data=sal1, x = analogNovelty.x, y = analogNovelty.y, aes(color = "blue")) +
+  geom_point(data=sal1, x = analogDisappearance.x, y = analogDisappearance.y, aes(color = "darkgreen")) +
+  xlim(-80, 80) + ylim(10, 80) +
+  theme(legend.position = bottom)  + 
+  scale_fill_manual(values = c('darkred', 'blue', 'darkgreen'), 
+                      labels = c('Current climate', 'Novel climate', 'Dissappearing climate'))
 
   
-sal
+sal      #legend is not working
 
 
 
+# Combine coords into one df (but can't connect points based on cells)
+current <- data.frame(long = sal1$long, lat = sal1$lat)
+current$climate <- "current"
+novel <- data.frame(long = sal1$analogNovelty.x, lat = sal1$analogNovelty.y)
+novel$climate <- "novel"
+dissappearing <- data.frame(long = sal1$analogDisappearance.x, lat = sal1$analogDisappearance.y)
+dissappearing$climate <- "dissappearing"
 
+dat <- rbind(current, novel, dissappearing) %>%
+  st_as_sf(coords = c("long", "lat"), crs = 4326)
 
+install.packages("wesanderson")
+library(wesanderson)
+names(wes_palettes)
 
-
-
+ggplot() + 
+  geom_map(
+    data = world, map = world,
+    aes(map_id = region),
+    color = "grey", fill = "lightgray", size = 0.01
+  ) + theme_bw()  +
+  xlim(-80, 80) + ylim(10, 80) +
+  geom_sf(data = dat, aes(color = climate)) +
+  labs(title = "Salmo salar SSP5-8.5", x = "Longitude", y = "Latitude") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  scale_colour_manual(values = wes_palette("GrandBudapest1", n = 3), name = "Climate", 
+                      labels = c("Current",  "Disappearing", "Novel"))
 
 
